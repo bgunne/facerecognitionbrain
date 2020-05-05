e613ec28e4d57f4e493c931dd1f3cdc14a445bde
Tamas Ujj <bgunne@gmail.com>
2020-05-05 11:35:07 +0200

    Updates

---
+++ precache-manifest.0583b8d3d2d8b6b7ce907d2a9ae566a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2954511c647a03e22eddc0403368a9c6",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "98b12dbb7a4c9bc4cacc",
+    "url": "/facerecognitionbrain/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "0da8fe771e14ad0ebe4e",
+    "url": "/facerecognitionbrain/static/css/main.332cb590.chunk.css"
+  },
+  {
+    "revision": "98b12dbb7a4c9bc4cacc",
+    "url": "/facerecognitionbrain/static/js/2.71677d27.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/facerecognitionbrain/static/js/2.71677d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0da8fe771e14ad0ebe4e",
+    "url": "/facerecognitionbrain/static/js/main.ebd17c27.chunk.js"
+  },
+  {
+    "revision": "406188be5b03bde0c247",
+    "url": "/facerecognitionbrain/static/js/runtime-main.55aaa9a4.js"
+  },
+  {
+    "revision": "5090b11d3b0bbb28ba319689d10d5671",
+    "url": "/facerecognitionbrain/static/media/brain.5090b11d.svg"
+  }
+]);
